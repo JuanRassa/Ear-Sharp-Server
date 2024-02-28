11ebd11dac906c28aff858922ab1334d38d5b800
Juan Rassa <jmrassa.dev@gmail.com>
2024-02-28 22:00:48 +0000

    Finish endpoit: PUT Edit User by Id.

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -2,6 +2,15 @@ const router = require('express').Router();
 const mongoose = require('mongoose');
 const User = require('../models/User.model');
 
+// ℹ️ Handles password encryption
+const bcrypt = require('bcrypt');
+
+// ℹ️ Handles password encryption
+const jwt = require('jsonwebtoken');
+
+// How many rounds should bcrypt run the salt (default - 10 rounds)
+const saltRounds = 10;
+
 const { isSuperAdmin, isOrgAdmin, isTeacher, isStudent } = require('../middleware/roles_checker.middlewares');
 const { isAuthenticated } = require('../middleware/jwt.middleware.js');
 
@@ -31,6 +40,43 @@ router.get('/users/:id', isAuthenticated, isSuperAdmin, async (req, res, next) =
     return res.status(200).json(user);
   } catch (error) {
     console.error(`An error occurred retrieving the referred user with id = ${id}`, error);
+    next(error);
+  }
+});
+
+router.put('/users/:id', isAuthenticated, isSuperAdmin, async (req, res, next) => {
+  const { id } = req.params;
+  const { name, last_name, username, email, password, is_super_admin, is_org_admin, is_teacher, is_student, exercises } = req.body;
+  try {
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(400).json({ message: 'Id is not valid. It must be of type: ObjectId.' });
+    }
+
+    // This regular expression checks password for special characters and minimum length
+    const passwordRegex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+    if (!passwordRegex.test(password)) {
+      res.status(400).json({
+        message: 'Password must have at least 6 characters and contain at least one number, one lowercase and one uppercase letter.',
+      });
+      return;
+    }
+
+    const salt = bcrypt.genSaltSync(saltRounds);
+    const hashedPassword = bcrypt.hashSync(password, salt);
+
+    const updatedUser = await User.findByIdAndUpdate(
+      id,
+      { name, last_name, username, email, password: hashedPassword, is_super_admin, is_org_admin, is_teacher, is_student, exercises },
+      { new: true }
+    );
+
+    if (!updatedUser) {
+      return res.status(404).json({ message: `User with id ${id} not found. No updates were made.` });
+    }
+
+    res.status(200).json({ message: 'User updated successfuly.', updatedUser: updatedUser });
+  } catch (error) {
+    console.error(`An error occurred retrieving/updating the referred user id = ${id}`, error);
     next(error);
   }
 });
